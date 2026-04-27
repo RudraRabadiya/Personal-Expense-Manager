d85af878414d8a4df665a45354d9434b68aedb04
Rudra-7127 <rudrarabadiya07@gmail.com>
2026-04-27 16:08:55 +0530

    hardcode CORS

--- PEM-Backend/app/main.py
+++ PEM-Backend/app/main.py
@@ -1,13 +1,12 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from app.config import settings
 from app.routes import auth, entries, udhar, admin, payments, reports
 
 app = FastAPI(title="KhataBook API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.origins_list,
+    allow_origins=["https://personal-expense-manager-phi.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -15,7 +14,7 @@ app.add_middleware(
 
 app.include_router(auth.router)
 app.include_router(entries.router)
-app.include_router(udhar.router)
+app.include_router(udhar.router)  
 app.include_router(admin.router)
 app.include_router(payments.router)
 app.include_router(reports.router)
