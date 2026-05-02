d5bff4aed6f097b8157c1fe0ee0bd643b74dbb9c
Rudra-7127 <rudrarabadiya07@gmail.com>
2026-05-02 17:57:29 +0530

    update main

--- PEM-Backend/app/main.py
+++ PEM-Backend/app/main.py
@@ -7,7 +7,9 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-allow_origins=[*],
+    allow_origins=[
+        "https://personal-expense-manager-gules.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
