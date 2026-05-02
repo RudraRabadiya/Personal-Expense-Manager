b22c4d88277a77a2ebf0bb1e4f823f14e143e5a0
Rudra-7127 <rudrarabadiya07@gmail.com>
2026-05-02 18:01:53 +0530

    fix: resolve merge conflict, update CORS

--- PEM-Backend/app/main.py
+++ PEM-Backend/app/main.py
@@ -3,24 +3,16 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.routes import auth, entries, udhar, admin, payments, reports
 
 import os
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-
-frontend_url = os.getenv("FRONTEND_URL", "http://localhost:5173")
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        frontend_url,
         "http://localhost:5173",
-<<<<<<< HEAD
-        "http://localhost:5174",
-        "http://localhost:5175",
-        "http://localhost:3000",
-        "personal-expense-manager-nhvvb9poc.vercel.app",
-=======
-        "https://personal-expense-manager-nhvvb9poc.vercel.app"
->>>>>>> 18e3dec (update main)
+        "https://personal-expense-manager-nhvvb9poc.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
