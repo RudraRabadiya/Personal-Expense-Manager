c64c87f7f673707c7d648439b7e374ff318b981a
VOID Reaper <rudrarabadiya07@gmail.com>
2026-05-02 17:52:31 +0530

    Update main.py

--- PEM-Backend/app/main.py
+++ PEM-Backend/app/main.py
@@ -16,7 +16,7 @@ app.add_middleware(
         "http://localhost:5174",
         "http://localhost:5175",
         "http://localhost:3000",
-        "https://your-frontend.up.railway.app",
+        "personal-expense-manager-nhvvb9poc.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
